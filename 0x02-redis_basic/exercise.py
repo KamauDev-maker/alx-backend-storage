9c3ba3984691909c0e8618d4a0a7f7582b22f8af
KamauDev-maker <oscarnjenga@gmail.com>
2023-10-18 18:50:24 +0000

    Incr values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,23 @@ Redis client module
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    count calls decorator takes a callable method as an arg
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Function that will replaxce the original method
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -18,6 +34,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Method that takes a data args and returns a str
